e5dcfa1fa5cde2d7844335f6a5788844f084dcc8
Sn4tch <alexis.leclercq@epitech.eu>
2021-10-25 20:05:36 +0200

    Get films data from TMDB + Add rows & banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import Banner from './Banner';
 import requests from './requests'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Banner/>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Animation" fetchUrl={requests.fetchAnimationMovies}/>
     </div>
   );
 }
